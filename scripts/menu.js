de3cb00578e3af6f1fb2724adf3c49b1a180e0de
kvrivera <heykatrivera@gmail.com>
2024-11-07 11:42:57 -0500

    fine-tuning hamburger button and main-menu nav responsiveness through css and js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,11 @@
 // responsive coding for the menu
+
+
+// hamburger button when in mobile view
+const hamButton = document.querySelector('#hamburger');
+const navigation = document.querySelector('#main-menu');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
