ff1fba62f367a2bc01116e43fa07e9367f0ac027
kvrivera <heykatrivera@gmail.com>
2024-11-21 16:43:18 -0500

    footer

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -12,6 +12,6 @@ let lastMod = document.querySelector("#lastModified");
 // convert the lastModified property into a date object:
 const dateLastMod = document.lastModified;
 // pick out only the Month, date, year, hours, mins, seconds
-document.getElementById("lastModified").innerHTML += `Last Modified: <span class="lastMod">${dateLastMod}</span>`
+document.getElementById("lastModified").innerHTML += `Last Modified: <span class="lastMod">${dateLastMod}</span>`;
 // why does this next line not work, instead of the line above?
 //lastMod.innerHTML = `<span class="lastMod">${dateLastMod.getFull()}</span>`
